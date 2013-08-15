4e78be574a908b931a554b9a3f7fa8ba2fa78358
Paras Mehta <pmehta.eu@googlemail.com>
2013-08-15 15:59:13 +0000

    web.js reads from index.html

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var mesg = fs.readFileSync('/index.html', "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
+
+app.get('/', function(request, response) {
+  response.send(mesg);});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
