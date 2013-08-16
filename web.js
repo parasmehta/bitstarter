70b6a48c38bbf3133db891037a9138444b33579f
Paras Mehta <pmehta.eu@googlemail.com>
2013-08-16 09:47:00 +0000

    Removed / before index.html in readfilesync in web.js

--- web.js
+++ web.js
@@ -3,9 +3,8 @@ var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
-
 app.get('/', function(request, response) {
-var mesg = fs.readFileSync('/index.html', "utf8", function (err, data) {
+var mesg = fs.readFileSync('index.html', "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
